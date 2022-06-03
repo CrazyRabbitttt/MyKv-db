e4e9b0a6efcd605a906764d3370957179c708d61
shaoguixin <2404987607@qq.com>
2022-06-03 14:07:09 +0800

    first commit:Slice

---
+++ Include/Slice.h
@@ -0,0 +1,98 @@
+#ifndef KVDB_INCLUDE_SLICE_H_
+#define KVDB_INCLUDE_SLICE_H_
+
+//Slice,而不使用String，因为String有默认的拷贝行为
+//并且不支持前缀的操作
+
+#include <cassert>
+#include <cstddef>
+#include <string>
+#include <cstring>
+#include "Include/Export.h"
+
+namespace kvdb {
+
+class LEVELDB_EXPORT Slice {
+ public:
+    //创建Empty slice
+    Slice():data_(""), size_(0) {}
+
+    //传递参数进行构造 d[0,n-1]
+    Slice(const char *d, size_t n) :data_(d), size_(n) {}
+
+    //传递String进行构造
+    Slice(const std::string& s) : data_(s.data()), size_(s.size()) {}
+
+    //默认的进行拷贝行为？
+    Slice(const Slice&) = default;
+    Slice& operator=(const Slice&) = default;
+
+    //返回一些private的参数等
+    size_t size() const {return size_;}
+
+    //Return true if length is zero
+    bool empty() const {return size_ == 0;}
+
+    //Return the data of Slice
+    const char* data() const {return data_;}
+
+    //重载[]
+    char operator[](size_t n) const {
+       assert(n < size());
+       return data_[n];
+    }
+
+    //Clear the Slice 
+    void Clear() {
+       data_ = "";
+       size_  = 0;
+    }
+
+    //去除掉前n个bytes
+    void remove_prefix(size_t n) {
+       assert(n <= size());
+       data_ += n;
+       size_ -= n;
+    }
+
+    //Return string type value 
+    std::string ToString() const {return std::string(data_, size_);}
+
+
+    //Comparation the Silce's 
+    int compare(const Slice& b) const;
+
+
+   //判断x是否是*this的前缀
+   bool starts_with(const Slice& x) const {
+      return((size_ >= x.size_) && (memcmp(data_, x.data_, x.size_) == 0));
+   }
+
+ private:
+    const char* data_;
+    size_t size_;
+};
+
+inline bool operator==(const Slice& x, const Slice& y) {
+   return ((x.size() == y.size()) && (memcmp(x.data(), y.data(), x.size()) == 0));
+}
+
+inline bool operator!=(const Slice& x, const Slice& y) const {
+   return !(x == y);    //调用上面声明了的重载的==
+}
+
+//compare two Slices
+inline int Slice::compare(const Slice& b) const {
+   const size_t len = (b.size_ < size_) ? b.size_ : size_;
+   int ret = memcmp(data_, b.data_, len);
+   //一个属于另一个的前缀，那么判断size的大小就可以了
+   if (ret == 0) {
+      if (size_ > b.size_) return 1;
+      else return -1;
+   }
+   return ret;
+}
+
+}   //end of the namespace kvdb
+
+#endif
