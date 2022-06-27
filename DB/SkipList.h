abc9a62e792a292994d6124a26654f32e816a349
shaoguixin <2404987607@qq.com>
2022-06-27 21:35:39 +0800

    完成了skipList的大致函数

--- DB/SkipList.h
+++ DB/SkipList.h
@@ -33,19 +33,39 @@ public:
     bool Contains(const Key& key) const;
 
 private:
-
-    Node* FindgreaterOrEqual(const Key& key, Node** prev) const;
-
     static const int kMaxHeight = 12;
 
 
+
+//Functions
+    //不保证并发性质的读取height
+    inline int GetMaxGeight() const {
+        return max_align_t.load(std::memory_order_relaxed);
+    }
+
+    bool Equal(const Key& a, const Key& b) const {
+        return (compare_(a, b) == 0);
+    }
+
+
+    //Return true if key > node->value 
+    bool KeyIsAfterNode(const Key& key, Node* node) const;
+
+    Node* FindgreaterOrEqual(const Key& key, Node** prev) const;
+
+    Node* NewNode(const Key& key, int height);
+
+
+//Varaiable 
     Comparatpr const compare_;
 
-    Arena* const arean_;        //用于进行node的内存的分配
+    Arena* const arena_;        //用于进行node的内存的分配
 
     Node* const header_;
 
     int RandomHeight();
+
+    std::atomic<int> max_height_;
 
     Random rand_;           //random 
 
@@ -99,6 +119,24 @@ private:
 
 
 
+template<typename Key, class Comparator> 
+typename SkipList<Key, Comparator>::Node* SkipList<Key, Comparator>::NewNode(const Key& key, int height) {
+    //因为最底层Node节点已经有了node* ，因此还是需要height -1 个
+    char* const node_mem = arena_->AllocateAligned(sizeof(Node) + sizeof(std::atomic<Node*>) * (height - 1));
+    return new(node_mem) Node(key);
+} 
+
+
+
+template<typename Key, class Comparator> 
+SkipList<Key, Comparator>::SkipList(Comparator cmp, Arena* arena) 
+    : compare_(cmp), arena_(arena), header_(NewNode(0, kMaxHeight)), max_height_(1), rand_(0xdeadbeef) {
+        //初始化所有的next
+        for (int i = 0; i < kMaxHeight; i++) {
+            header_->SetNext(i, nullptr);
+        }
+    }
+
 
 
 //获得随机的高度，随机进行高度的增长，最高12层
@@ -117,36 +155,43 @@ int SkipList<Key, Comparator>::RandomHeight() {
     return height;
 }
 
+
+
+
+//每层查找最后一个小于Key的节点
 template<typename Key, class Comparator> 
-typename SkipList<Key, Comparator>::Node* 
+typename SkipList<Key, Comparator>::Node*               //typename 用在这里来指明后面接的是一种类型
+SkipList<Key, Comparator>::FindgreaterOrEqual(const Key& key, Node** prev) const {
+    //双重指针就相当于单指针的数组
+    Node* pNode = header_;               //begin from header node 
+    int level = GetMaxGeight() - 1;    //从最高层次开始
+    for (int i = level; i >= 0; i--) {
+        while (KeyIsAfterNode(key, pNode->Next(i))) {    //在目前层次的下一个
+            pNode = pNode -> next_[i];
+        }
+    }   
+    return nullptr;
+}
+
+//判断Key的值是不是大于node的值
+template<typename Key, class Comparator> 
+bool SkipList<Key, Comparator>::KeyIsAfterNode(const Key& key, Node* node) const {
+    return (n != nullptr && compare_(n->key, key) < 0);
+}
 
 
 
 
 
-
-
-
-// template<typename Key, class Comparator>
-// bool SkipList<Key, Comparator>::Contains(const Key& key) const {
-    
-// }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+template<typename Key, class Comparator>
+bool SkipList<Key, Comparator>::Contains(const Key& key) const {
+    Node* tmp = FindgreaterOrEqual(key, nullptr);
+    if (x != nullptr && Equal(key, tmp->key)) {
+        return true;
+    } else {
+        return false;
+    }
+}
 
 }
 
